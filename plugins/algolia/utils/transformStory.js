b18c3c2595c331032982731f8fded5eb8c1d23b8
manuelJung <manu.jung@relaxcommerce.com>
2019-06-28 16:04:02 +0200

    created markdown story transformation

--- plugins/algolia/utils/transformStory.js
+++ plugins/algolia/utils/transformStory.js
@@ -1,6 +1,24 @@
+const {Converter} = require('showdown')
+
+
 
 module.exports = function transformStory(story){
   if(!story) return {dict:{},grids:{}}
+
+  story.COMPONENTS.forEach(component => {
+    if(component.name === 'Text'){
+      component.props.children = converter.makeHtml(component.props.children)
+    }
+    if(component.name === 'TextWithImage'){
+      component.props.textProps.children = converter.makeHtml(component.props.textProps.children)
+    }
+    if(component.name === 'TextWithImageGallery'){
+      component.props.textProps.children = converter.makeHtml(component.props.textProps.children)
+    }
+    if(component.name === 'TextWithEmbedly'){
+      component.props.textProps.children = converter.makeHtml(component.props.textProps.children)
+    }
+  })
 
   const byGridName = story.COMPONENTS.reduce((dict, next) => (dict[next.props.gridArea]=next) && dict, {})
 
@@ -15,6 +33,18 @@ module.exports = function transformStory(story){
   }
 }
 
+const converter = new Converter({
+  tables: true,
+  tablesHeaderId: true,
+  literalMidWordUnderscores: true,
+  extensions: [
+    {
+      type: 'output',
+      regex: new RegExp('--(.+)--', 'g'),
+      replace: '<i class="fa fa-$1"></i>'
+    }
+  ]
+})
 
 // return {
 //   dict: {
