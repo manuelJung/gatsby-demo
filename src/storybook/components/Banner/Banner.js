1f8ab291d45f9c1008f3f72bdcc7647c7863922d
manuelJung <manu.jung@relaxcommerce.com>
2019-07-08 17:16:34 +0200

    init Banner component

---
+++ src/storybook/components/Banner/Banner.js
@@ -0,0 +1,28 @@
+// @flow
+import * as React from 'react'
+import styled from 'styled-components'
+
+
+type Props = {
+  src: string,
+  alt: string,
+  link: string,
+  title: string,
+  subtitle: string,
+  overlayColor: string,
+  overlayPosition: string,
+  ratio: string,
+  ratioSubtitle: string
+}
+
+export default function Banner (props:Pros) {
+  return (
+    <Wrapper className='Banner'>
+      <img src={props.src} alt={props.alt} />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  > img {width:100%;}
+`
