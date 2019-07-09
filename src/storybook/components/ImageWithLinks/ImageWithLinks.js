2f3046f430bd3dd1dc9f5f5688f252d620899b31
manuelJung <manu.jung@relaxcommerce.com>
2019-07-09 18:08:04 +0200

    updated ImageWithLinks

--- src/storybook/components/ImageWithLinks/ImageWithLinks.js
+++ src/storybook/components/ImageWithLinks/ImageWithLinks.js
@@ -1,13 +1,31 @@
 // @flow
 import * as React from 'react'
-// import Img from 'gatsby-image'
+import styled from 'styled-components'
+import useLazyImageSrc from 'hooks/useLazyImageSrc'
 
+type Props = {
+  alt: string,
+  src: string,
+  context: {
+    base64: string
+  }
+}
 
+export default function ImageWithLinks ({alt,src,context}:Props) {
+  const [ref, image] = useLazyImageSrc(src, context.base64)
 
-export default function ImageWithLinks ({src}) {
   return (
-    <div className='Image'>
-      <h3>IMAGE-WITH-LINKS</h3>
-    </div>
+    <Wrapper className='ImageWithLinks'>
+      <div className='image-wrapper'>
+        <img ref={ref} src={image} alt={alt}/>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  > .image-wrapper {
+    width: 100%;
+    > img { width: 100%; }
+  }
+`
