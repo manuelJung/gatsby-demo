f82fbb90d81cfafdb18b3e70b6e85968035cbccf
manuelJung <manu.jung@relaxcommerce.com>
2019-07-15 22:51:20 +0200

    init storybook

--- src/storybook/components/Banner/Banner.story.js
+++ src/storybook/components/Banner/Banner.story.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 storiesOf('Button', module)
   .add('with text', () => <button onClick={action('clicked')}>Hello Button</button>)
