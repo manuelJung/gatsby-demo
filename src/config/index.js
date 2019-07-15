80914734716004ecf6aab2018a6df45d9f2e2424
manuelJung <manu.jung@relaxcommerce.com>
2019-07-15 23:00:21 +0200

    exposed config to storybook

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 
-const app_mode = process.env.APP_MODE || process.env.GATSBY_APP_MODE
+const app_mode = process.env.APP_MODE || process.env.GATSBY_APP_MODE || process.env.STORYBOOK_APP_MODE
 const config = require(`./${app_mode}`).default
 export default config
--- src/storybook/components/Banner/Banner.story.js
+++ src/storybook/components/Banner/Banner.story.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import config from 'config'
 
 storiesOf('Button', module)
-  .add('with text', () => <button onClick={action('clicked')}>Hello Button</button>)
+  .add('with text', () => <button onClick={action('clicked')}>Hello Button {config.app_name}</button>)
   .add('with some emoji', () => (
     <button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
