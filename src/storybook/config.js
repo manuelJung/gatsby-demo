9958004f82780bbff60ab88e514efe1d33e60705
manuelJung <manu.jung@relaxcommerce.com>
2019-07-23 14:58:41 +0200

    add panel to the right

--- src/storybook/config.js
+++ src/storybook/config.js
@@ -1,8 +1,13 @@
-import { configure } from '@storybook/react'
+import { configure, addParameters } from '@storybook/react' 
+
 const req = require.context('./components', true, /.story.js/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
 
+addParameters({
+  options: {panelPosition: 'right'}
+})
 configure(loadStories, module)
+
