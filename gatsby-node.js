18b988418c3c902888d0e4c51575dac6041aacde
manuelJung <manu.jung@relaxcommerce.com>
2019-07-30 22:35:30 +0200

    export all from gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,12 +3,4 @@ require("babel-register")({
   plugins: ["transform-object-rest-spread"]
 })
 require('babel-polyfill')
-const node = require('./gatsby-node.babel')
-
-exports.createPages = node.createPages
-exports.createSchemaCustomization = node.createSchemaCustomization
-exports.sourceNodes = node.sourceNodes
-exports.onCreatePage = node.onCreatePage
-
-
-
+module.exports = require('./gatsby-node.babel')
